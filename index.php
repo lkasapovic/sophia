d121de1665d8d17b665f2b3f5270d43f719560aa
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-05-18 10:44:49 -0700

    no message

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>SOPHIA BUSH</title>
+         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.css"> 
+        <link type="text/css" rel="stylesheet" href="css/custom-style.css">
     </head>
     <body>
         <?php

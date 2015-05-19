7b63f9aadd0eba3d9f634f9799aa594b84905819
Lejla Kaasapovic <lakergirl.lk@gmail.com>
2015-05-19 09:19:58 -0700

    no message

--- index.php
+++ index.php
@@ -2,14 +2,17 @@
     <head>
         <meta charset="UTF-8">
         <title>SOPHIA BUSH</title>
-         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link type="text/css" rel="stylesheet" href="css/bootstrap-theme.css">
         <link type="text/css" rel="stylesheet" href="css/bootstrap.css"> 
         <link type="text/css" rel="stylesheet" href="css/custom-style.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="jumbotron">
+            <h1>SOPHIA BUSH</h1>
+            <h2>Pasadena, California</h2>
+            <p><a id="a" class="btn btn-lg" role="button" href="https://www.google.com/search?q=sophia+bush&oq=sophia+bush&aqs=chrome..69i57j69i61j69i59j69i60j0l2.2186j0j7&sourceid=chrome&es_sm=93&ie=UTF-8">Google page</a></p>
+        </div>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
